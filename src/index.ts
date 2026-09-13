27733c0a215df528cd257e7de32589827dca113f
artiphishle <fabio.gartenmann@protonmail.com>
2026-09-13 07:08:04 +0200

    feat: initialize hetzner adapter foundation

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+/** Public Hetzner Cloud compute adapter package boundary. */
+export { infraAdapterDescriptor } from './constants/infra';
+export { createInfraAdapter } from './features/cloud-compute/composition/createInfraAdapter';
